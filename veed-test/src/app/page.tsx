7641ec03ab275375496af8642968c00ca2c76a64
berk <badday182@gmail.com>
2025-04-08 14:54:47 +0300

    add get data

--- veed-test/src/app/page.tsx
+++ veed-test/src/app/page.tsx
@@ -1,5 +1,7 @@
+import Posts from "@/components/posts";
 import QueryProvider from "@/components/QueryProvider";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Home() {
   return (
@@ -7,11 +9,14 @@ export default function Home() {
       <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
           <p>hello</p>
-          <Button>Click me</Button>
+          <Posts />
+
+          {/* <Button variant="primary">Click me</Button> */}
         </main>
         {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer> */}
       </div>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryProvider>
   );
 }
