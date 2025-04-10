9b4fbbd91e1567a4d880b828f3bbba76f49625c9
berk <badday182@gmail.com>
2025-04-10 11:37:19 +0300

    add initial form

--- veed-test/src/app/page.tsx
+++ veed-test/src/app/page.tsx
@@ -1,14 +1,18 @@
 import ToDosList from "@/components/todos";
 import QueryProvider from "@/components/QueryProvider";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "sonner";
+import { AddTodoForm } from "@/components/addTodoForm/addTodoForm";
 
 export default function Home() {
   return (
     <QueryProvider>
       <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col w-full items-center">
+          <AddTodoForm />
           <ToDosList />
         </main>
+        <Toaster />
       </div>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryProvider>
---
+++ veed-test/src/components/addTodoForm/addTodoForm.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+// import { toast } from "@/components/hooks/use-toast";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const FormSchema = z.object({
+  task: z.string().min(2, {
+    message: "Task must be at least 2 characters.",
+  }),
+});
+
+export function AddTodoForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      task: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast("You added the following task:", {
+      description: <p>{JSON.stringify(data.task)}</p>,
+    });
+    form.reset();
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="task"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Task</FormLabel>
+              <FormControl>
+                <Input placeholder="Text New Task" {...field} />
+              </FormControl>
+              <FormDescription>Add New Task.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Add</Button>
+      </form>
+    </Form>
+  );
+}
