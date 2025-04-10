0529922bac8444023e53937a997624c722909e16
berk <badday182@gmail.com>
2025-04-10 10:15:41 +0300

    refactor: extract TodoCard component from ToDosList for better code organization

---
+++ veed-test/src/components/todoCard/TodoCard.tsx
@@ -0,0 +1,40 @@
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Trash } from "lucide-react";
+
+import { Todo } from "@/types";
+import clsx from "clsx";
+
+// const TodoCard = (todo: Todo) => {
+const TodoCard = ({ id, completed, title }: Todo) => {
+  return (
+    <Card key={id} className={clsx("shadow-md min-w-xs max-w-3xl w-full")}>
+      <CardHeader className="pb-2 grow">
+        <CardTitle className="text-lg flex gap-2 items-baseline  ">
+          <Checkbox id={`todo-${id}`} checked={completed} />
+          <span
+            className={completed ? "line-through text-muted-foreground" : ""}
+          >
+            {title}
+          </span>
+        </CardTitle>
+        <CardDescription>Task #{id}</CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <Badge variant={completed ? "default" : "secondary"}>
+          {completed ? "Completed" : "Pending"}
+        </Badge>
+        <Trash className="ml-auto text-red-500" />
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default TodoCard;
--- veed-test/src/components/todos/index.tsx
+++ veed-test/src/components/todos/index.tsx
@@ -1,21 +1,8 @@
 "use client";
 
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Badge } from "@/components/ui/badge";
-import { Checkbox } from "@/components/ui/checkbox";
-
-import { Trash } from "lucide-react";
-
-import styles from "./index.module.css";
-import clsx from "clsx";
 import { Todo } from "@/types";
 import useGetToDos from "@/hooks/useGetToDos";
+import TodoCard from "@/components/todoCard/TodoCard";
 
 const ToDosList = () => {
   const isEnableForListRender = true; // робити запит в жалежності від потреби
@@ -26,32 +13,7 @@ const ToDosList = () => {
       {isLoading ? (
         <div className="text-center py-8">Loading...</div>
       ) : data?.length ? (
-        data.map((todo: Todo) => (
-          <Card
-            key={todo.id}
-            className={clsx(styles.brs, "shadow-md min-w-xs max-w-3xl w-full")}
-          >
-            <CardHeader className="pb-2 grow">
-              <CardTitle className="text-lg flex gap-2 items-baseline  ">
-                <Checkbox id={`todo-${todo.id}`} checked={todo.completed} />
-                <span
-                  className={
-                    todo.completed ? "line-through text-muted-foreground" : ""
-                  }
-                >
-                  {todo.title}
-                </span>
-              </CardTitle>
-              <CardDescription>Task #{todo.id}</CardDescription>
-            </CardHeader>
-            <CardFooter>
-              <Badge variant={todo.completed ? "default" : "secondary"}>
-                {todo.completed ? "Completed" : "Pending"}
-              </Badge>
-              <Trash className="ml-auto text-red-500" />
-            </CardFooter>
-          </Card>
-        ))
+        data.map((todo: Todo) => <TodoCard {...todo} key={todo.id} />)
       ) : isError ? (
         <div className="text-center py-8">No tasks found</div>
       ) : (
